eb6de2b4d30fcdb5e5ae9f8e9180fb9faef84da7
sahilanna <sahil.mehar@nineleaps.com>
2023-04-12 15:27:18 +0530

    update working fine

--- src/Components/Dashboard/PM/pmCreate.js
+++ src/Components/Dashboard/PM/pmCreate.js
@@ -6,21 +6,18 @@ import { useNavigate } from 'react-router-dom';
 
  function PmCreate(){
 let navigate= useNavigate();
-const[pmId,setpmId]=useState('');
-const[pmName,setpmName]=useState('')
 
-  const [projectId, setProjectId] = useState('');
-  const [projectName, setProjectName] = useState('');
-  const [projectDesc, setProjectDesc] = useState('');
-  console.log(projectId);
-  console.log(projectName);
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const[enumRole,setEnumRole]=useState('2');
   const sendDataToAPI = () => {
     axios.post(`https://6429847d5a40b82da4d494b2.mockapi.io/PM`, {
-        pmId,
-        pmName,
-      projectId,
-      projectName,
-      projectDesc
+        id,
+        name,
+      email,
+      enumRole
+      
     }).then(() => {
       navigate('/pmRead')
     })
@@ -29,24 +26,22 @@ const[pmName,setpmName]=useState('')
 <Form>
 <Form.Field>
         <label>Project-Manager ID</label>
-        <input name='pmId' onChange={(e)=>setpmId(e.target.value)} placeholder='PM Id' />
+        <input name='id' onChange={(e)=>setId(e.target.value)} placeholder='PM Id' />
     </Form.Field>
     <Form.Field>
         <label>Project-Manager Name</label>
-        <input name='pmName' onChange={(e)=>setpmName(e.target.value)} placeholder='PM Name' />
+        <input name='name' onChange={(e)=>setName(e.target.value)} placeholder='PM Name' />
     </Form.Field>
     <Form.Field>
-        <label>Project-Id</label>
-        <input name='ProjectId' onChange={(e)=>setProjectId(e.target.value)} placeholder='ProjectId' />
+        <label>Email-ID</label>
+        <input type='email' name='email' onChange={(e)=>setEmail(e.target.value)} placeholder='EMAIL' />
     </Form.Field>
+    
     <Form.Field>
-      <label>Project-Name</label>
-      <input name='ProjectName' onChange={(e)=>setProjectName(e.target.value)} placeholder='ProjectName' />
+    <label>Role : Project Manager </label>
     </Form.Field>
-    <Form.Field>
-      <label>Project-Description</label>
-      <input name='ProjectName' onChange={(e)=>setProjectDesc(e.target.value)} placeholder='ProjectDescription' />
-    </Form.Field>
+      
+     
     {/* <Form.Field> */}
       {/* <Checkbox label='I agree to the Terms and Conditions' /> */}
     {/* </Form.Field> */}
